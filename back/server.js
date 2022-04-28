d684725c4ee4d0dd94dfbb2703a7b9d2f82fd21f
i-Lucas <novo.contato.lucas@gmail.com>
2022-04-28 10:12:53 -0300

    update express and mongo connection

--- back/server.js
+++ back/server.js
@@ -1,11 +1,19 @@
 import express from "express";
-import cors from "cors";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const server = express();
 server.use(cors(), express.json());
+
 server.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+});
+
+const mongoClient = new mongodb.MongoClient(process.env.MONGO_PORT);
+let db;
+
+mongoClient.connect().then(() => {
+	db = mongoClient.db("meu_lindo_projeto");
 });
